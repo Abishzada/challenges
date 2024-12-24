75ce1d3912e4d9bbc479679159b7afe7d31c77f7
Abishzada Rustam <r.abishzadeh@gmail.com>
2024-12-24 13:51:22 +0400

    boyner moore method added / naivest method added

--- findIndexofFirstStr.js
+++ findIndexofFirstStr.js
@@ -7,3 +7,41 @@ var strStr = function (haystack, needle) {
   }
   return -1;
 };
+
+// var strStr = function (haystack, needle) {
+//     return haystack.indexOf(needle)
+// };
+
+
+//////// Boyner Moore Algorithm ////////
+// var strStr = function (haystack, needle) {
+//     const m = needle.length;
+//     const n = haystack.length;
+
+//     if (m === 0) return 0;
+
+//     const badChar = {};
+
+//     for (let i = 0; i < m; i++) {
+//         badChar[needle[i]] = i;
+//     }
+
+//     let s = 0; // shift 
+//     while (s <= n - m) {
+//         let j = m - 1; // Start comparing from end 
+
+//         while (j >= 0 && needle[j] === haystack[s + j]) {
+//             j--;
+//         }
+
+//         // If j becomes -1
+//         if (j < 0) {
+//             return s; // Return starting 
+//         } else {
+//             // Calculate shift 
+//             const badCharShift = badChar[haystack[s + j]] !== undefined ? badChar[haystack[s + j]] : -1;
+//             s += Math.max(1, j - badCharShift);
+//         }
+//     }
+//     return -1;
+// };
